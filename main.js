40df118c002ab6442570a2c98e05761978bc6fe0
Niek van Delft <guest12085@gmail.com>
2023-03-10 18:25:11 +0100

    slksdjkl

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-async function search(link) {
+async function getLinks(link, i) {
+    i++
     let out = []
-    response = fetch(`https://en.wikipedia.org/w/api.php?action=parse&page=${link}&format=json&origin=*`)
-        .then((response) => response.json())
-        .then((data) => {
-            data.parse.links.forEach(link => {
-                out.push(link['*'])
-            });
-            output = out
-            return output
-        });
+    let response = await fetch(`https://en.wikipedia.org/w/api.php?action=parse&page=${link}&format=json&origin=*`)
+    let data = await response.json();
+    data.parse.links.forEach(link => {
+        out.push(link['*'])
+    });
+    // console.log(out, i)
+    return out
+}
+
+async function doubleTrouble() {
+    let links = await getLinks('pet-door', 0)
+    for (i = 0; i < links.length; i++) {
+        const link = links[i]
+        const output = await getLinks(link, 1)
+        console.log(output)
+    }
 }
